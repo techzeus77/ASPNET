f31c820b52e84cf4abb68fff671227a000c1f3ae
techzeus77 <techzeus777@gmail.com>
2021-03-03 09:14:17 -0800

    so far so good, step 11"

---
+++ Testing/Models/product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Testing.Models
+{
+    public class product
+    {
+        public product() 
+        { 
+
+        }
+
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int CategoryID { get; set; }
+        public int OnSale { get; set; }
+        public int StockLevel { get; set; }
+
+
+
+
+
+
+
+
+
+    }
+}
